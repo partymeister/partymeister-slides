f93a9d77f000446615830aff362a819c97d2796a
Reza Esmaili <bitbucket@dfox.info>
2021-04-26 22:27:19 +0200

    Add PlaylistItemRequest

--- src/Http/Requests/Backend/PlaylistItemRequest.php
+++ src/Http/Requests/Backend/PlaylistItemRequest.php
@@ -6,20 +6,90 @@ use Motor\Backend\Http\Requests\Request;
 
 /**
  * Class PlaylistItemRequest
+ *
  * @package Partymeister\Slides\Http\Requests\Backend
  */
 class PlaylistItemRequest extends Request
 {
-
     /**
      * @OA\Schema(
      *   schema="PlaylistItemRequest",
      *   @OA\Property(
-     *     property="name",
-     *     type="string",
-     *     example="Example data"
+     *     property="playlist_id",
+     *     type="integer",
+     *     example="1"
      *   ),
-     *   required={"name"},
+     *   @OA\Property(
+     *     property="type",
+     *     type="string",
+     *     example="image"
+     *   ),
+     *   @OA\Property(
+     *     property="slide_type",
+     *     type="string",
+     *     example="announce"
+     *   ),
+     *   @OA\Property(
+     *     property="slide_id",
+     *     type="integer",
+     *     example="1"
+     *   ),
+     *   @OA\Property(
+     *     property="duration",
+     *     type="integer",
+     *     example="20"
+     *   ),
+     *   @OA\Property(
+     *     property="transition_id",
+     *     type="integer",
+     *     example="1"
+     *   ),
+     *   @OA\Property(
+     *     property="slide_slidemeister_id",
+     *     type="integer",
+     *     example="1"
+     *   ),
+     *   @OA\Property(
+     *     property="transition_duration",
+     *     type="integer",
+     *     example="2000"
+     *   ),
+     *   @OA\Property(
+     *     property="is_advanced_manually",
+     *     type="boolean",
+     *     example="false"
+     *   ),
+     *   @OA\Property(
+     *     property="is_muted",
+     *     type="boolean",
+     *     example="false"
+     *   ),
+     *   @OA\Property(
+     *     property="midi_note",
+     *     type="integer",
+     *     example="203"
+     *   ),
+     *   @OA\Property(
+     *     property="metadata",
+     *     type="json",
+     *     example="{}"
+     *   ),
+     *   @OA\Property(
+     *     property="callback_hash",
+     *     type="string",
+     *     example="102030405060708090"
+     *   ),
+     *   @OA\Property(
+     *     property="callback_delay",
+     *     type="integer",
+     *     example="20"
+     *   ),
+     *   @OA\Property(
+     *     property="sort_position",
+     *     type="integer",
+     *     example="1"
+     *   ),
+     *   required={"playlist_id"},
      * )
      */
 
@@ -33,7 +103,6 @@ class PlaylistItemRequest extends Request
         return true;
     }
 
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -42,7 +111,21 @@ class PlaylistItemRequest extends Request
     public function rules()
     {
         return [
-
+            'playlist_id'                => 'required|integer',
+            'type'                       => 'nullable',
+            'slide_type'                 => 'nullable',
+            'slide_id'                   => 'nullable|integer',
+            'duration'                   => 'nullable|integer',
+            'transition_id'              => 'nullable|integer',
+            'transition_slidemeister_id' => 'nullable|integer',
+            'transition_duration'        => 'nullable|integer',
+            'is_advanced_manually'       => 'nullable|boolean',
+            'is_muted'                   => 'nullable|boolean',
+            'midi_note'                  => 'nullable|integer',
+            'metadata'                   => 'nullable|json',
+            'callback_hash'              => 'nullable',
+            'callback_delay'             => 'nullable|integer',
+            'sort_position'              => 'nullable|integer',
         ];
     }
 }
