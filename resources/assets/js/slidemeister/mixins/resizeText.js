580d8f1ff3c4f1ee7d078745555a89d3d1c35f85
Reza Esmaili <github@dfox.info>
2025-04-15 20:42:15 +0200

    Add delay and linux-specific font-rendering handling

--- resources/assets/js/slidemeister/mixins/resizeText.js
+++ resources/assets/js/slidemeister/mixins/resizeText.js
@@ -1,14 +1,15 @@
 export default {
     methods: {
         resizeText(target, width, height) {
-            if (this.readonly) {
-                this.$forceNextTick(() => {
+            setTimeout(() => {
+                if (this.readonly) {
+                    this.$forceNextTick(() => {
+                        this.doResize(target, width, height);
+                    });
+                } else {
                     this.doResize(target, width, height);
-                });
-            } else {
-                this.doResize(target, width, height);
-            }
-
+                }
+            }, 250);
         },
         doResize(target, width, height) {
             if (!width) {
@@ -19,12 +20,19 @@ export default {
             }
 
             let textElement = target.querySelector('div');
+
             while ((textElement.offsetWidth < width && textElement.offsetHeight < height) && parseInt(textElement.style.fontSize.replace('px', '')) <= parseInt(textElement.dataset.fontSize)) {
                 textElement.style.fontSize = (parseInt(textElement.style.fontSize.replace('px', '')) + 1) + 'px';
                 target.style.fontSize = textElement.style.fontSize;
             }
 
             while ((textElement.offsetWidth > width || textElement.offsetHeight > height) && parseInt(textElement.style.fontSize.replace('px', '')) > 5) {
+                textElement.style.fontSize = (parseInt(textElement.style.fontSize.replace('px', '')) - 1) + 'px';
+                target.style.fontSize = textElement.style.fontSize;
+            }
+
+            // Separate adjustment because linux renders fonts differently which might cause problems
+            if ((height - textElement.offsetHeight) < 10 ) {
                 textElement.style.fontSize = (parseInt(textElement.style.fontSize.replace('px', '')) - 1) + 'px';
                 target.style.fontSize = textElement.style.fontSize;
             }
