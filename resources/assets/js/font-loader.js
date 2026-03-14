3259fc83fb607a6184460e88a6c4d7c14f4b9716
Reza Esmaili <github@dfox.info>
2026-03-14 03:13:05 +0100

    Remove deprecated Vue 2 slidemeister code + medium-editor SCSS
    Delete partymeister-slidemeister-web/, slidemeister/, and
    font-loader.js — all replaced by Vue 3 slidemeister-web and
    slidemeister-editor.

--- resources/assets/js/font-loader.js
+++
@@ -1,1 +0,0 @@
-window.WebFont = require('webfontloader');
--- resources/assets/js/slidemeister/mixins/elementNameHelper.js
+++
@@ -1,7 +0,0 @@
-export default {
-    methods: {
-        createElementName() {
-            return Math.floor((Math.random() * 100000000) + 1);
-        },
-    }
-};
